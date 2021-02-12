d2cc7d0885cebc9c92c44b21409ed72890c2b4d2
Dlamente <alexisgiubi320@gmail.com>
2021-02-12 20:19:35 -0300

    primeraversion

---
+++ Readme.md
@@ -0,0 +1,8 @@
+This is my web repository built from scratch, without some external help, to demonstrate what I can create from the client side (frontend). I am a person who constantly educates myself, to get ahead, I am an enthusiastic coder, not just a university student, I want to go beyond where no one could go, and achieve what few were able to achieve.
+
+
+Modification of 2021
+
+contact: Alexisgiubi320@gmail.com // +595981621895
+
+Copying the source code, Copyright
--- main.js
+++ main.js
@@ -7,3 +7,69 @@ ScrollReveal().reveal('.news-cards', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+switch(val){
+case 1:
+  answer = "alpha";
+  break;
+caso 2:
+  answer = "beta";
+  break;
+  case 3:
+    answer = "gamma";
+  break;
+  case 4:
+    answer = "delta";
+    break;
+           }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
